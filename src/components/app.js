c653deb4bf7d8b0c4cb3aed0483039900c8968bd
chris482 <christrinh5@gmail.com>
2019-11-24 20:09:38 -0800

    Configure webpack

--- src/components/app.js
+++ src/components/app.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { Switch, Route } from "react-router-dom";
 
-import Splash from '../components/splash/splash';
+import Splash from './splash/splash';
 
 
 const App = () => (
