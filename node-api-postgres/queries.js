252ae08a7c1352d7b0dbbd3a39ef5d89a7923d97
Chris Trinh <christrinh5@gmail.com>
2019-11-26 12:58:21 -0800

    Connect postgresql to node

---
+++ node-api-postgres/queries.js
@@ -0,0 +1,17 @@
+const Pool = require('pg').Pool
+const pool = new Pool({
+  user: 'me',
+  host: 'localhost',
+  database: 'api',
+  password: 'password',
+  port: 5432,
+})
+
+const getUsers = (request, response) => {
+    pool.query('SELECT * FROM users ORDER BY id ASC', (error, results) => {
+      if (error) {
+        throw error
+      }
+      response.status(200).json(results.rows)
+    })
+  }
