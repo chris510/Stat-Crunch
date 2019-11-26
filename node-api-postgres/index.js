8abc5f7c0510bf78d3a1d56de03dd10d4855d747
Chris Trinh <christrinh5@gmail.com>
2019-11-26 14:38:18 -0800

    Add dependencies for node postgres

--- node-api-postgres/index.js
+++ node-api-postgres/index.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const db = require('./queries');
+
 const app = express();
 const port = process.env.PORT || 3000;
 
@@ -17,3 +19,5 @@ app.get('/', (req, res) => {
 app.listen(port, () => {
     console.log(`App running on port ${port}.`)
 })
+
+app.get('/shots', db.getShots);
--- node-api-postgres/queries.js
+++ node-api-postgres/queries.js
@@ -1,17 +1,22 @@
 const Pool = require('pg').Pool
+
 const pool = new Pool({
-  user: 'me',
+  user: 'chris',
   host: 'localhost',
-  database: 'api',
+  database: 'nba-shots-db_development',
   password: 'password',
   port: 5432,
 })
 
-const getUsers = (request, response) => {
-    pool.query('SELECT * FROM users ORDER BY id ASC', (error, results) => {
+const getShots = (request, response) => {
+  pool.query('SELECT * FROM shots', (error, results) => {
       if (error) {
-        throw error
+        throw error;
+      } else {
+        response.status(200).json(results.rows)
       }
-      response.status(200).json(results.rows)
-    })
-  }
+  })
+};
+
+  
+module.exports = getShots;
